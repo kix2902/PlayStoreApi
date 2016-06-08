229e8880eb0cfc596049e15e268142cacbf6f294
Ismael Reyes <ismael.kix2902@gmail.com>
2016-06-08 13:11:34 +0200

    Added getApp function

--- library/data/app.php
+++ library/data/app.php
@@ -9,6 +9,18 @@ class App implements \JsonSerializable
     private $price;
     private $url;
 
+    private $description;
+    private $category;
+    private $rating_value;
+    private $rating_count;
+
+    private $file_size;
+    private $date_updated;
+    private $num_downloads;
+    private $version;
+    private $required_android;
+    private $content_rating;
+
     public function getPackage()
     {
         return $this->package;
@@ -79,6 +91,91 @@ class App implements \JsonSerializable
     {
         $url = trim($url);
         $this->url = $url;
+    }
+
+    public function getDescription(){
+        return $this->description;
+    }
+
+    public function setDescription($description){
+        $this->description = $description;
+    }
+
+    public function getCategory(){
+        return $this->category;
+    }
+
+    public function setCategory($category){
+        $category = trim($category);
+        $this->category = $category;
+    }
+
+    public function getRatingValue(){
+        return $this->rating_value;
+    }
+
+    public function setRatingValue($rating_value){
+        $this->rating_value = $rating_value;
+    }
+
+    public function getRatingCount(){
+        return $this->rating_count;
+    }
+
+    public function setRatingCount($rating_count){
+        $this->rating_count = $rating_count;
+    }
+
+    public function getFileSize(){
+        return $this->file_size;
+    }
+
+    public function setFileSize($file_size){
+        $file_size=trim($file_size);
+        $this->file_size = $file_size;
+    }
+
+    public function getDateUpdated(){
+        return $this->date_updated;
+    }
+
+    public function setDateUpdated($date_updated){
+        $this->date_updated = $date_updated;
+    }
+
+    public function getNumDownloads(){
+        return $this->num_downloads;
+    }
+
+    public function setNumDownloads($num_downloads){
+        $num_downloads=trim($num_downloads);
+        $this->num_downloads = $num_downloads;
+    }
+
+    public function getVersion(){
+        return $this->version;
+    }
+
+    public function setVersion($version){
+        $version=trim($version);
+        $this->version = $version;
+    }
+
+    public function getRequiredAndroid(){
+        return $this->required_android;
+    }
+
+    public function setRequiredAndroid($required_android){
+        $required_android=trim($required_android);
+        $this->required_android = $required_android;
+    }
+
+    public function getContentRating(){
+        return $this->content_rating;
+    }
+
+    public function setContentRating($content_rating){
+        $this->content_rating = $content_rating;
     }
 
     public function JsonSerialize()
